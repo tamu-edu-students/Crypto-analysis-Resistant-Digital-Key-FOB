a1abfc1479ae036b73d0fbd7bd5d5bd8a8a38532
IronHammer2002 <jjenshein@gmail.com>
2023-09-23 18:40:59 -0500

    App Rev 2

--- DigitalKeyFOB/app/src/main/java/com/example/digitalkeyfob/CommandCenter.kt
+++ DigitalKeyFOB/app/src/main/java/com/example/digitalkeyfob/CommandCenter.kt
@@ -2,10 +2,12 @@ package com.example.digitalkeyfob
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
 
-class NewProfileActivity : AppCompatActivity(){
+class CommandCenter : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.newprofile) // Set the layout for the secondary activity
+        setContentView(R.layout.command_center)
     }
 }
--- DigitalKeyFOB/app/src/main/java/com/example/digitalkeyfob/MainActivity.kt
+++ DigitalKeyFOB/app/src/main/java/com/example/digitalkeyfob/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.digitalkeyfob
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val newprofileButton = findViewById<Button>(R.id.AddProfile)
+
+        // Set a click listener for the button
+        newprofileButton.setOnClickListener {
+            // Create an Intent to launch the SecondaryActivity
+            val intent = Intent(this, NewProfileActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
