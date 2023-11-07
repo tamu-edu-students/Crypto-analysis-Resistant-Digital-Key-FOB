ee35636b96771ab7570040822f993b64f0b0bff5
ceaton24 <144845137+ceaton24@users.noreply.github.com>
2023-11-07 14:27:33 -0600

    Weekly Updates

--- Courtney/CCSubsystemCodeforDevice/app/src/main/java/com/example/ccsubsystemcodefordevice/AdvancedEncryptionStandard.kt
+++ Courtney/CCSubsystemCodeforDevice/app/src/main/java/com/example/ccsubsystemcodefordevice/AdvancedEncryptionStandard.kt
@@ -14,6 +14,14 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.ccsubsystemcodefordevice.ui.theme.CCSubsystemCodeForDeviceTheme
+//Needed Imports Below
+import javax.crypto.Cipher
+import java.util.Base64
+import javax.crypto.spec.IvParameterSpec
+import javax.crypto.spec.PBEKeySpec
+import javax.crypto.SecretKeyFactory
+import javax.crypto.spec.SecretKeySpec
+
 
 class AdvancedEncryptionStandard : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,7 +43,30 @@ class AdvancedEncryptionStandard : ComponentActivity() {
 @Composable
 fun AESEncryption(modifier: Modifier = Modifier) {
     Surface(color = Color.LightGray) {
-        Text(text = "Testing: ", modifier = modifier.padding(12.dp))
+        //Setting up variables for the AES Encryption
+        val SecretKeyInt = 221 //SecretKey from the Diffie-Hellman Key Exchange
+        val SecretKey = SecretKeyInt.toString()
+        val ivbytes = byteArrayOf(0x64, 0x7b, 0x5e, 0x57, 0x67, 0x1f, 0x2c, 0x10, 0x43, 0x25, 0x0a, 0x25, 0x72, 0x12, 0x49, 0x03)
+        val salt = "CrytoAnalysisDigtialKeyFOB"
+        val zHardware = byteArrayOf(0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f)
+
+        //IV Random Generator (May need to fix this if it messes with the IV value)
+        val ivspec = IvParameterSpec(ivbytes);
+
+        //Generating a secret key from Diffie-Hellman Output
+        val factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256")
+        val spec = PBEKeySpec(SecretKey.toCharArray(), salt.toByteArray(), 65536, 256)
+        val sk_temp = factory.generateSecret(spec)
+        val symmetrickey = SecretKeySpec(sk_temp.getEncoded(), "AES")
+
+        //Doing the encryption
+        val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
+        cipher.init(Cipher.ENCRYPT_MODE, symmetrickey, ivspec)
+        val cipherText = cipher.doFinal(zHardware)
+        val CipherTextString = Base64.getEncoder().encodeToString(cipherText)
+
+        Text(text = "Testing: \nKey = $symmetrickey \n\nCipher Text = $CipherTextString",
+            modifier = modifier.padding(12.dp))
     }
 }
 
